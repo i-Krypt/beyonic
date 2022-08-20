b10ec892c8ab846f402d02890115ce1e97ac3db1
i-Krypt <vamay505@gmail.com>
2022-08-21 01:50:28 +0300

    global colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+         'brand-blue': '#00456B',
+         'brand-blue-lighter': '#006FAB',
+         'brand-blue-lightest': '#CAECFF',
+         'semantic-green': '#008F50'
+      }
+    },
   },
   plugins: [],
 }
